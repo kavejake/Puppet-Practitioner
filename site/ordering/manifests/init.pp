1de118b0f0eef4662b3e3b631e66b819061532bd
kyle <kyle@puppetlabs.vm>
2017-10-09 22:32:18 +0000

    Update 'site/ordering/manifests/init.pp'

--- site/ordering/manifests/init.pp
+++ site/ordering/manifests/init.pp
@@ -2,7 +2,16 @@ class ordering {
   # include the ordering::mysql class
   # Does it need to be contained?
 
-  notify { 'This should come after the entire MySQL class is enforced':
-    require => Class['ordering::mysql'],
-  }
+#  notify { 'This should come after the entire MySQL class is enforced':
+#    require => Class['ordering::mysql'],
+#  }
+    
+
+      contain ordering::mysql
+
+      notify { 'This should come after the entire MySQL class is enforced':
+        require => Class['ordering::mysql'],
+      }
+    }
+
 }
