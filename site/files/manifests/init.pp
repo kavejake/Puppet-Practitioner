8627e16e17565a2bf1917962b45cf6ee225cd427
kyle <kyle@puppetlabs.vm>
2017-10-09 22:42:19 +0000

    Update 'site/files/manifests/init.pp'

--- site/files/manifests/init.pp
+++ site/files/manifests/init.pp
@@ -1,10 +1,11 @@
 class files {
+
   File {
     owner => 'root',
     group => 'root',
     mode  => '0644',
   }
-  file { '/etc/cron.allow':
+  file { ['/etc/cron.allow','/etc/cron.deny']:
     ensure => file,
   }
   file_line { 'allow root cron jobs':
@@ -12,17 +13,29 @@ class files {
     path   => '/etc/cron.allow',
     line   => 'root',
   }
-  # Add a rule to cron.deny to deny jobs by default
 
+  file_line { 'deny all cron jobs':
+    ensure => present,
+    path   => '/etc/cron.deny',
+    line   => '*',
+  }
 
-  # What concat resource is needed for this fragment to work?
+  concat { '/etc/motd':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+  }
+
   concat::fragment { 'motd header':
     target  => '/etc/motd',
     order   => '01',
     content => epp('files/motd_header.epp'),
   }
 
-  # Add a few fragments to be appended to /etc/motd
-
+  concat::fragment { 'motd warning':
+    target  => '/etc/motd',
+    order   => '02',
+    content => 'Warning! You have accessed a restricted area!',
+  }
 
 }
